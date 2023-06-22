1672c412da6a0903ed9ada5106aa7b53b8355a3c
ueha-j <uehara.junji@ntt-tx.co.jp>
2023-06-22 16:23:56 +0900

    initial

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import { Refine } from '@refinedev/core';
+import routerBindings, {
+  NavigateToResource,
+  UnsavedChangesNotifier,
+} from '@refinedev/react-router-v6';
+import dataProvider from '@refinedev/simple-rest';
+import {
+  notificationProvider,
+  RefineThemes,
+  ThemedLayoutV2,
+  ErrorComponent,
+} from '@refinedev/mantine';
+import { NotificationsProvider } from '@mantine/notifications';
+import { MantineProvider, Global } from '@mantine/core';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import { MantineInferencer } from '@refinedev/inferencer/mantine';
+
+const App = () => {
+  return (
+    <MantineProvider
+      theme={RefineThemes.Blue}
+      withNormalizeCSS
+      withGlobalStyles>
+      <Global styles={{ body: { WebkitFontSmoothing: 'auto' } }} />
+      <NotificationsProvider position='top-right'>
+        <BrowserRouter>
+          <Refine
+            routerProvider={routerBindings}
+            dataProvider={dataProvider('https://api.fake-rest.refine.dev')}
+            notificationProvider={notificationProvider}
+            resources={[
+              {
+                name: 'blog_posts',
+                list: '/blog-posts',
+                show: '/blog-posts/show/:id',
+                create: '/blog-posts/create',
+                edit: '/blog-posts/edit/:id',
+              },
+            ]}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+            }}>
+            <Routes>
+              <Route
+                element={
+                  <ThemedLayoutV2>
+                    <Outlet />
+                  </ThemedLayoutV2>
+                }>
+                <Route
+                  index
+                  element={<NavigateToResource resource='blog_posts' />}
+                />
+                <Route path='blog-posts'>
+                  <Route index element={<MantineInferencer />} />
+                  <Route path='show/:id' element={<MantineInferencer />} />
+                  <Route path='edit/:id' element={<MantineInferencer />} />
+                  <Route path='create' element={<MantineInferencer />} />
+                </Route>
+                <Route path='*' element={<ErrorComponent />} />
+              </Route>
+            </Routes>
+            <UnsavedChangesNotifier />
+          </Refine>
+        </BrowserRouter>
+      </NotificationsProvider>
+    </MantineProvider>
+  );
+};
+
+export default App;
